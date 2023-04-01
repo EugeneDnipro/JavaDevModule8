87ad19a3fee9044566b8e01336633660fd2713dc
EugeneDnipro <eugenednepr@gmail.com>
2023-04-01 14:56:16 +0300

    Added UTC time zone option

--- src/main/java/org/example/TimeServlet.java
+++ src/main/java/org/example/TimeServlet.java
@@ -5,27 +5,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.Instant;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.TimeZone;
 
 @WebServlet("/time")
 public class TimeServlet extends HttpServlet {
-
-    private String initTime;
-
-    @Override
-    public void init() {
-        initTime = Instant.now().toString();
-    }
+    String timezone;
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html; charset=utf-8");
-        resp.getWriter().write(initTime);
+        timezone = req.getParameter("timezone");
+        resp.getWriter().write(getCurrentUtcTime(timezone));
         resp.getWriter().close();
     }
 
-    @Override
-    public void destroy() {
-        initTime = null;
+    public static String getCurrentUtcTime(String timezone) {
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+//        sdf.setTimeZone(TimeZone.getTimeZone("timezone"));
+//        return sdf.format(new Date());
+        if (timezone == null) {
+            timezone = "UTC";
+        } else if (timezone.contains(" ")) {
+            timezone = timezone.replace(" ", "+");
+        }
+        ZoneId zone = ZoneId.of(timezone);
+        LocalDateTime localTime = LocalDateTime.now();
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(zone);
+        return zonedDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss '" + timezone + "'"));
     }
+
 }
