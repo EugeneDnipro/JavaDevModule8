9caa5fb75700fb515ab6a69fb9dd887ead28d11c
EugeneDnipro <EugeneDnepr@gmail.com>
2023-04-04 13:31:22 +0300

    Remove warnings

--- src/main/java/org/example/TimezoneValidateFilter.java
+++ src/main/java/org/example/TimezoneValidateFilter.java
@@ -7,8 +7,6 @@ import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.ZoneId;
-import java.time.zone.ZoneRulesException;
 import java.util.Arrays;
 import java.util.TimeZone;
 
@@ -16,7 +14,7 @@ import java.util.TimeZone;
 public class TimezoneValidateFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        String timeZoneId = null;
+        String timeZoneId;
         String timezone = req.getParameter("timezone");
         if (timezone == null) {
             timeZoneId = "UTC";
